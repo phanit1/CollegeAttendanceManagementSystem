d0eae635d35d0a03b54f43a627a3b1a6306984bc
Phaneendhar Thota <phaneendhar.thota@valuemomentum.com>
2024-01-09 06:51:19 +0530

    All files added

---
+++ src/app/app.component.ts
@@ -0,0 +1,131 @@
+import { Component } from '@angular/core';
+import * as xlsx from 'xlsx';
+import { StudentData, StudentsData, StudentAttendance } from './studentdata.model';
+// import excelfile from "../../SampleRecords.xlsx"
+import { DatePipe } from '@angular/common';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'CollegeAttendanceManagementSystem';
+  exceldata: StudentsData[] = [];
+  selectedDepartment: string = '';
+  selectedYear: string = '';
+  selectedSection: string = '';
+  selectedpinnumber: string = '';
+  attendancereport: any;
+  studentsdata: any;
+  currentDate:any;
+  attendancestatus: boolean = false;
+  studattend: StudentAttendance[] = [];
+  serverurl: string = "https://college-attendance-system-backend.vercel.app/"
+  constructor(private datePipe: DatePipe) {
+    this.currentDate = this.datePipe.transform(new Date(), 'yyyy/MM/dd');
+  }
+  ngOnInit() {
+  }
+ 
+  getattendancereport() {
+    let requestOptions: RequestInit = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    fetch(this.serverurl+"studentattendancereport/" + this.selectedpinnumber, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        console.log(result);
+        this.attendancereport = JSON.parse(result);
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  submitForm() {
+    let requestOptions: RequestInit = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    fetch(this.serverurl+"students/" + this.selectedYear + "/" + this.selectedSection + "/" + this.selectedDepartment, requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        // console.log(result);
+        if(result != "No Student Data Found") {
+          this.studentsdata = JSON.parse(result);
+        }
+        else {
+          this.studentsdata = result;
+        }
+        this.studattend = []
+        this.default_attendance()
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  default_attendance() {
+    this.studentsdata.forEach((student:any) => {
+      let studattendobj = new StudentAttendance();
+      studattendobj.PinNumber = student.PinNumber;
+      studattendobj.Date = this.currentDate;
+      studattendobj.AttendanceStatus = false;
+      this.studattend.push(studattendobj);
+    });
+    console.log(this.studattend,"default")
+  }
+
+  updateOrAddEntry(data: any[], newEntry: any): any[] {
+    const updatedData: any[] = [];
+    let updated = false;
+
+    data.forEach(entry => {
+      if (entry.PinNumber === newEntry.PinNumber && entry.Date === newEntry.Date) {
+        // Update the previous entry
+        updatedData.push(newEntry);
+        updated = true;
+      } else {
+        updatedData.push(entry);
+      }
+    });
+
+    // If not updated, add the new entry
+    if (!updated) {
+      updatedData.push(newEntry);
+    }
+    console.log(updatedData)
+    return updatedData;
+  }
+
+  mark_attendance(pinno: string, event: any) {
+    // console.log(this.studentsdata,"StudData")
+    let studattendobj = new StudentAttendance();
+    studattendobj.PinNumber = pinno;
+    studattendobj.Date = this.currentDate;
+    studattendobj.AttendanceStatus = event.srcElement.checked;
+    this.studattend = this.updateOrAddEntry(this.studattend, studattendobj);
+    
+  }
+  sendAttendance() {
+var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(this.studattend);
+
+    var requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch(this.serverurl+"poststudentsattendance", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+  }
+}
---
+++ src/app/studentdata.model.ts
@@ -0,0 +1,17 @@
+export class StudentData {
+    PinNumber: string="";
+    StudentName: string = "";
+    Department: string = "";
+    Year:string="";
+    Section:string="";
+    ParentPhoneNumber!:number;
+}
+
+export class StudentsData {
+    studentData : StudentData[] = [];
+}
+export class StudentAttendance {
+    PinNumber :string = "";
+    Date:string = "";
+    AttendanceStatus:boolean = false;
+}
